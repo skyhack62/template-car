09109912d8d83a8d389de9f0cfb198391a338404
skyhack62 <157430911+skyhack62@users.noreply.github.com>
2024-01-23 01:44:07 +0100

    tempplate-car/__resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,24 +2,24 @@
 resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'
 
 files {
-	'data/carcols.meta',
-	'data/carvariations.meta',
-	'data/handling.meta',
-	'data/vehicles.meta',
+	'carcols.meta',
+	'carvariations.meta',
+	'handling.meta',
+	'vehicles.meta',
     --'data/contentunlocks.meta',
     --'data/vehiclelayouts.meta',
     --'data/shop_vehicle.meta',
 }
 
 
-data_file 'CARCOLS_FILE' 'data/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
-data_file 'HANDLING_FILE' 'data/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 
 --data_file 'CONTENT_UNLOCKING_META_FILE' 'data/contentunlocks.meta'
 --data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
 --data_file 'VEHICLE_SHOP_DLC_FILE' 'data/shop_vehicle.meta'
 
 
---client_script { 'client/vehicle_name.lua' }
+client_script { 'client/vehicle_name.lua' }
